f703933594fd5afdd93011c2275e996b637b0763
TJ Sornet <tsornet@gmail.com>
2019-10-23 20:21:59 -0700

    Added initial styling for navbar links

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -1,15 +1,19 @@
 import React from "react";
+import "./stylesheet/navbar.css";
 
 export default function Navbar(props) {
   return (
-    <div>
-      {props.items.map(item => {
-        return (
-          <a key={item.section} href="/">
-            {item.label}
-          </a>
-        );
-      })}
+    <div className="navbar">
+      <nav className="nav-items">
+        {props.items.map(item => {
+          return (
+            <a key={item.section} href="/">
+              {item.label}
+            </a>
+          );
+        })}
+      </nav>
+      <hr />
     </div>
   );
 }
