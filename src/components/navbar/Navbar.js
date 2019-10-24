92df5d9ddf37860e2603bf6b38463377e698c8cd
TJ Sornet <tsornet@gmail.com>
2019-10-23 21:44:02 -0700

    Added focus styling

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -2,18 +2,22 @@ import React from "react";
 import "./stylesheet/navbar.css";
 
 export default function Navbar(props) {
+  const handleClick = (e) => {
+    e.preventDefault();
+  }
+
   return (
     <div className="navbar">
       <nav className="nav-items">
         {props.items.map(item => {
           return (
-            <a key={item.section} href="/">
+            <a key={item.section} href="/" onClick={handleClick} tabIndex={0}>
               {item.label}
             </a>
           );
         })}
       </nav>
-      <hr />
+      <div className="bottom-border" />
     </div>
   );
 }
